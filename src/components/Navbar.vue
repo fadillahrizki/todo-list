692ec968d036ac60427ce4490feb2347bf376eb1
fadillahrizki <fadillahrizki16@gmail.com>
2022-10-23 22:36:23 +0700

    refactor: remove fixed navbar

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -10,9 +10,6 @@
         padding:2rem 12rem;
         box-shadow: 0 10px 15px -3px rgb(0 0 0 / .1),
             0 4px 6px -4px rgb(0 0 0 / .1);
-        position: fixed;
-        width:100%;
-        top:0;
     }
     .navbar h2 {
         font-size:1.5rem;
